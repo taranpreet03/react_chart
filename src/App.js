f65857bcaa48f6ef17975b4d742331c2a5f03433
taranpreet03 <taranpreet.kaur@omlogistics.co.in>
2026-05-04 12:36:31 +0530

    Line/Grid,Lengend,Tooltip

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import {
   Line,
   XAxis,
   YAxis,
-  CartesianGrid
+  CartesianGrid,Legend,Tooltip
 } from 'recharts';
 
 export const shipmentTrends = [
   { date: "Mon", created: 120, delivered: 95 },
-  { date: "Tue", created: 150, delivered: 110 },
-  { date: "Wed", created: 175, delivered: 140 },
-  { date: "Thu", created: 170, delivered: 160 },
+  { date: "Tues", created: 150, delivered: 110 },
+  { date: "Wednes", created: 175, delivered: 140 },
+  { date: "Thurs", created: 170, delivered: 160 },
   { date: "Fri", created: 20, delivered: 175 },
-  { date: "Sat", created: 140, delivered: 130 },
+  { date: "Satur", created: 140, delivered: 130 },
   { date: "Sun", created: 90, delivered: 85 },
 ];
 
@@ -25,21 +25,24 @@ function App() {
 
       <ResponsiveContainer width="100%" aspect={5}>
         <LineChart data={shipmentTrends} width={300} height={300} margin={{top:5,right:300,left:300,bottom:5}}>
-          <CartesianGrid strokeDasharray={"3 3"}/>
-          <XAxis dataKey="date" />
-          <YAxis />
+          <CartesianGrid strokeDasharray={"8 8"}/>
+          <Legend/>
+          <XAxis dataKey="date"  tickFormatter={(value=>value+"day")}/>
+          <YAxis /><Tooltip/>
           <Line
             type="monotone"
             dataKey="created"
-            stroke="#4E67DD"
+            stroke="#e24141"
             strokeWidth={3}
+            activeDot={{r:9}}
           />
 
           <Line
             type="monotone"
             dataKey="delivered"
-            stroke="#22C55E"
+            stroke="#1d8f35"
             strokeWidth={3}
+            activeDot={{r:9}}
           />
 
         </LineChart>
